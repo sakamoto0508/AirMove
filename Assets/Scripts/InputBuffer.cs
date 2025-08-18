eba3a1ee05b716cbbf5066c907b6a10e0538abb8
sakamoto0508s12 <sakamoto0508s12@gmail.com>
2025-08-19 02:22:10 +0900

    [add]inputbuffer

--- Assets/Scripts/InputBuffer.cs
+++ Assets/Scripts/InputBuffer.cs
@@ -1,16 +1,40 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(PlayerInput))]
 public class InputBuffer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    private const string MOVE_ACTION = "Move";
+    private const string LOOK_ACTION = "Look";
+    private const string JUMP_ACTION = "Jump";
+    private const string SPRINT_ACTION = "Sprint";
+    private const string CROUCH_ACTION = "Crouch";
+    private const string SLIDING_ACTION = "Sliding";
 
-    // Update is called once per frame
-    void Update()
+    public InputAction MoveAction => _moveAction;
+    public InputAction LookAction => _lookAction;
+    public InputAction JumpAction => _jumpAction;
+    public InputAction SprintAction => _sprintAction;
+    public InputAction CrouchAction => _crouchAction;
+    public InputAction SlidingAction => _slidingAction;
+
+    private InputAction _moveAction;
+    private InputAction _lookAction;
+    private InputAction _jumpAction;
+    private InputAction _sprintAction;
+    private InputAction _crouchAction;
+    private InputAction _slidingAction;
+
+    private void Awake()
     {
-        
+        if (TryGetComponent<PlayerInput>(out var playerInput))
+        {
+            _moveAction = playerInput.actions[MOVE_ACTION];
+            _lookAction = playerInput.actions[LOOK_ACTION];
+            _jumpAction = playerInput.actions[JUMP_ACTION];
+            _sprintAction = playerInput.actions[SPRINT_ACTION];
+            _crouchAction = playerInput.actions[CROUCH_ACTION];
+            _slidingAction = playerInput.actions[SLIDING_ACTION];
+        }
     }
 }

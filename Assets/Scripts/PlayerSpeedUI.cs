845fe52cb31386ac10b1902cf6a30b4b3575f885
sakamoto0508s12 <sakamoto0508s12@gmail.com>
2025-08-19 18:28:51 +0900

    [add]playerspeedUI

---
+++ Assets/Scripts/PlayerSpeedUI.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerSpeedUI : MonoBehaviour
+{
+    [SerializeField] private Text _playerSpeedText;
+    private Rigidbody _rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_rb != null )
+        {
+            // 水平速度のみを計算（Y軸を除く）
+            Vector3 horizontalVelocity = new Vector3(_rb.linearVelocity.x, 0, _rb.linearVelocity.z);
+            float speed = horizontalVelocity.magnitude;
+            // 速度を表示（小数点1桁まで）
+            _playerSpeedText.text = $"速度: {speed:F1} m/s";
+        }
+    }
+}

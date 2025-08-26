2736d7ebad9a7a9dbf68d9104d52ab3a75e332a7
sakamoto0508s12 <sakamoto0508s12@gmail.com>
2025-08-26 11:02:22 +0900

    [add]statemachine

--- Assets/Scripts/PlayerState.cs
+++ Assets/Scripts/PlayerState.cs
@@ -7,7 +7,38 @@ public class PlayerState : MonoBehaviour
     {
         walking,
         sprinting,
+        wallrunning,
         crouching,
         sliding,
+        air
+    }
+
+    public void StateMachine(bool isWallRunning,bool isSliding,bool isCrouching,bool isGround,bool isSlope, bool isSprinting)
+    {
+        if(isWallRunning)
+        {
+            CurrentState = State.wallrunning;
+            return;
+        }
+        if (isSliding)
+        {
+            CurrentState = State.sliding;
+        }
+        else if (isCrouching)
+        {
+            CurrentState = State.crouching;
+        }
+        else if ((isGround||isSlope) && isSprinting)
+        {
+            CurrentState = State.sprinting;
+        }
+        else if ((isGround||isSlope) && !isSprinting)
+        {
+            CurrentState = State.walking;
+        }
+        else
+        {
+            CurrentState = State.air;
+        }
     }
 }
